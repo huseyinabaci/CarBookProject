05664ccbf5a60b1343079deb90c6ed239f8aec18
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-02-21 21:29:44 +0300

    Testimonial Partial

---
+++ CarBook.Application/Features/CQRS/Handlers/TestimonialHandlers/GetTestimonialByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Application.Features.CQRS.Queries.TestimonialQueries;
+using CarBook.Application.Features.CQRS.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.TestimonialHandlers
+{
+    public class GetPricingByIdQueryHandler : IRequestHandler<GetTestimonialByIdQuery, GetTestimonialByIdQueryResult>
+    {
+        private readonly IRepository<Testimonial> _repository;
+        public GetPricingByIdQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetTestimonialByIdQueryResult> Handle(GetTestimonialByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetTestimonialByIdQueryResult
+            {
+                TestimonialID = values.TestimonialID,
+                Name = values.Name,
+                Comment = values.Comment,
+                Title = values.Title,
+                ImageUrl = values.ImageUrl
+            };
+        }
+    }
+}
---
+++ CarBook.Application/Features/CQRS/Handlers/TestimonialHandlers/RemoveTestimonialCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Application.Features.CQRS.Commands.TestimonialCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.TestimonialHandlers
+{
+    public class RemovePricingCommandHandler : IRequestHandler<RemoveTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public RemovePricingCommandHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
